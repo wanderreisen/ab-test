83c39a9199f5ed54acd329427577e107966a3faf
Konstantin Knopp <k.knopp@das-unikat.com>
2019-12-12 14:16:52 +0100

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All notable changes to `laravel-ab` will be documented in this file
+
+## 0.0.1 - 2019-02-07
+
+- initial release
---
+++ tests/GoalTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Ben182\AbTesting\Tests;
+
+use Ben182\AbTesting\AbTesting;
+use Ben182\AbTesting\AbTestingFacade;
+use Illuminate\Support\Facades\Event;
+use Ben182\AbTesting\Events\GoalCompleted;
+
+class GoalTest extends TestCase
+{
+    public function test_that_goal_complete_works()
+    {
+        $returnedGoal = AbTestingFacade::completeGoal('firstGoal');
+
+        $experiment = session(AbTesting::SESSION_KEY_EXPERIMENT);
+        $goal = $experiment->goals->where('name', 'firstGoal')->first();
+
+        $this->assertEquals($goal, $returnedGoal);
+
+        $this->assertEquals(1, $goal->hit);
+
+        $this->assertEquals(collect([$goal->id]), session(AbTesting::SESSION_KEY_GOALS));
+
+        Event::assertDispatched(GoalCompleted::class, function ($g) use ($goal) {
+            return $g->goal->id === $goal->id;
+        });
+    }
+
+    public function test_that_goal_can_only_be_completed_once()
+    {
+        $this->test_that_goal_complete_works();
+
+        $experiment = session(AbTesting::SESSION_KEY_EXPERIMENT);
+        $goal = $experiment->goals->where('name', 'firstGoal')->first();
+
+        $this->assertEquals(1, $goal->hit);
+
+        $returnedGoal = AbTestingFacade::completeGoal('firstGoal');
+
+        $this->assertFalse($returnedGoal);
+
+        $this->assertEquals(1, $goal->hit);
+
+        $this->assertEquals(collect([$goal->id]), session(AbTesting::SESSION_KEY_GOALS));
+    }
+
+    public function test_that_invalid_goal_name_returns_false()
+    {
+        $this->assertFalse(AbTestingFacade::completeGoal('1234'));
+    }
+
+    public function test_that_completed_goals_works()
+    {
+        AbTestingFacade::completeGoal('firstGoal');
+
+        $experiment = session(AbTesting::SESSION_KEY_EXPERIMENT);
+        $goal = $experiment->goals->where('name', 'firstGoal');
+
+        $this->assertEquals($goal->pluck('id')->toArray(), AbTestingFacade::getCompletedGoals()->pluck('id')->toArray());
+    }
+}
